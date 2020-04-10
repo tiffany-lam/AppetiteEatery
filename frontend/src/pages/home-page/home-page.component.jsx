467281ce8f3acd195087ed2f57f45a77be05891a
at235am <at235am@gmail.com>
2020-04-09 17:56:20 -0700

    some more set up

--- frontend/src/pages/home-page/home-page.component.jsx
+++ frontend/src/pages/home-page/home-page.component.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import "./home-page.styles.scss";
 
 const HomePage = () => {
-  return <div className="home-page-container">OUR HOME PAGE</div>;
+  return <div className="home-page-container">Home Page</div>;
 };
 
 export default HomePage;
