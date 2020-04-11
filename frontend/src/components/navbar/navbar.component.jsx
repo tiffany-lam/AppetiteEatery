4754ea086df6939bf7b05f56dfccc400d4a6d133
at235am <at235am@gmail.com>
2020-04-10 22:27:34 -0700

    first draft of navbar complete

--- frontend/src/components/navbar/navbar.component.jsx
+++ frontend/src/components/navbar/navbar.component.jsx
@@ -10,19 +10,27 @@ class Navbar extends React.Component {
   render() {
     return (
       <nav>
-        <Logo className="logo" />
+        <div className="logo-container-flex">
+          <Link to="/">
+            <Logo />
+          </Link>
+        </div>
 
         <ul>
           <li>
+            <div className="circle-mask"></div>
             <Link to="/">Home</Link>
           </li>
           <li>
+            <div className="circle-mask"></div>
             <Link to="/graduated">Graduated</Link>
           </li>
           <li>
+            <div className="circle-mask"></div>
             <Link to="/profile">Profile</Link>
           </li>
           <li>
+            <div className="circle-mask"></div>
             <Link to="/login">Login</Link>
           </li>
         </ul>
