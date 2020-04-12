c26b12fb4424f60ac9da21fd04461a5b89ec1e18
at235am <at235am@gmail.com>
2020-04-11 17:55:41 -0700

    navbar is now responsive and refractored some components out (CircleButton)

--- frontend/src/components/logo/logo.component.jsx
+++ frontend/src/components/logo/logo.component.jsx
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from "react";
 
+// custom components:
+import CircleButton from "../circle-btn/circle-btn.component";
+
 // icons:
 import LocalDiningIcon from "@material-ui/icons/LocalDining";
 import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
@@ -22,7 +25,11 @@ const Logo = ({ className, eVersion = 0, uppercase = false }) => {
 
       <span id="ap-text">ap</span>
       <span id="petite-text">p{letterE[eVersion]}tite</span>
-      <LocalDiningIcon id="fork-knife-icon" />
+
+      <CircleButton hoverRotate={true} id="fork-knife-icon">
+        <LocalDiningIcon />
+      </CircleButton>
+
       {/* <FiberManualRecordIcon id="small-dot-icon" /> */}
       <span id="eatery-text">eatery</span>
     </p>
--- frontend/src/components/navbar/navbar.component.jsx
+++ frontend/src/components/navbar/navbar.component.jsx
@@ -1,43 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 // custom components:
 import Logo from "../logo/logo.component";
+import CircleButton from "../circle-btn/circle-btn.component";
+
+// mui icons:
+import MenuIcon from "@material-ui/icons/Menu";
 
 // custom stylesheet:
 import "./navbar.styles.scss";
 
-class Navbar extends React.Component {
-  render() {
-    return (
-      <nav>
-        <div className="logo-container-flex">
-          <Link to="/">
-            <Logo eVersion={1} uppercase={false} />
-          </Link>
-        </div>
+const Navbar = ({ className }) => {
+  const [hideNav, setHideNav] = useState(true);
 
-        <ul>
-          <li>
-            <div className="circle-mask"></div>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <div className="circle-mask"></div>
-            <Link to="/graduated">Graduated</Link>
-          </li>
-          <li>
-            <div className="circle-mask"></div>
-            <Link to="/profile">Profile</Link>
-          </li>
-          <li>
-            <div className="circle-mask"></div>
-            <Link to="/login">Login</Link>
-          </li>
-        </ul>
-      </nav>
-    );
-  }
-}
+  const toggleNavBar = () => {
+    setHideNav(!hideNav);
+  };
+  return (
+    <nav>
+      <div className="logo-container-flex">
+        <Link to="/">
+          <Logo eVersion={1} uppercase={true} />
+        </Link>
+      </div>
+
+      <CircleButton id="menu-btn" onClick={toggleNavBar} hoverRotate={true}>
+        <MenuIcon />
+      </CircleButton>
+
+      <ul className={hideNav ? "mobile-hidden" : ""}>
+        {/* <ul> */}
+        <li>
+          <div className="nav-item-mask"></div>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <div className="nav-item-mask"></div>
+          <Link to="/graduated">Graduated</Link>
+        </li>
+        <li>
+          <div className="nav-item-mask"></div>
+          <Link to="/profile">Profile</Link>
+        </li>
+        <li>
+          <div className="nav-item-mask"></div>
+          <Link to="/login">Login</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Navbar;
