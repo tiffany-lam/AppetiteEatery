e0927ebc2925f83299ec2546d14bfffa7867aa39
Veronica Sumariyanto <Veronica.Sumariyanto@student.csulb.edu>
2020-04-13 05:18:50 -0700

    Added placeholders and some styling

--- frontend/src/pages/profile-page/profile-page.component.jsx
+++ frontend/src/pages/profile-page/profile-page.component.jsx
@@ -11,25 +11,40 @@ class ProfilePage extends Component {
         this.state = {
             name: "Tom",
             userName: "wonton-tom",
-            city: "San Francisco",
-            state: "CA",
+            location: "San Francisco, CA",
             joinDate: "1/27/19",
             placesVisited: "23",
-            reviews: "14"
+            reviews: "14",
+            map: "https://www.massive.pr/wp-content/uploads/2018/01/shutterstock_127728257-1038x576-tender-1024x568.jpg",
+            photo: "https://images.unsplash.com/photo-1504544750208-dc0358e63f7f?ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80"
         }
     }
 
     render() {
         return (
-            <div className="userInfo">
-                <span className="info">{this.state.name}</span>
-                <span className="info">{this.state.userName}</span>
-                <span className="info">{this.state.city}</span>
-                <span className="info">{this.state.state}</span>
-                <span className="info">{this.state.joinDate}</span>
-                <span className="info">{this.state.placesVisited}</span>
-                <span className="info">{this.state.reviews}</span>
-            </div>
+            <React.Fragment>
+                <div className="page-background">
+                    <div className="userInfo">
+                        <ul>
+                            <li>Name: {this.state.name}</li>
+                            <li>Username: {this.state.userName}</li>
+                            <li>City: {this.state.location}</li>
+                            <li>Member Since: {this.state.joinDate}</li>
+                            <li>Places Visited: {this.state.placesVisited}</li>
+                            <li>Reviews: {this.state.reviews}</li>
+                            <li><img class="map-img" src={this.state.map} alt="map"/></li>
+                        </ul>
+                    </div>
+                    <div className="userReviews">
+                        <ul>
+                            <li>Restaurant Name: Wonton-a-Bay </li>
+                            <li>Review Comment: My favorite Wonton place in the whole wide world! </li>
+                            <li>Date Posted: </li>
+                            <li><img class="review-img" src={this.state.photo} alt="map"/></li>
+                        </ul>
+                    </div>
+                </div>
+            </React.Fragment>
         )
     }
 }
