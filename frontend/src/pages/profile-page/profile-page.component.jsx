bf7147f3e845fd7ceee9a040cdfeeb4660a24cbb
Veronica Sumariyanto <Veronica.Sumariyanto@student.csulb.edu>
2020-04-15 11:40:35 -0700

    added extra review placeholder

--- frontend/src/pages/profile-page/profile-page.component.jsx
+++ frontend/src/pages/profile-page/profile-page.component.jsx
@@ -16,6 +16,7 @@ class ProfilePage extends Component {
             joinDate: "1/27/19",
             placesVisited: "23",
             reviews: "14",
+            favorites: "wontons, tacos, ice cream, fruit smoothies",
             profilePic: "https://images.unsplash.com/photo-1489481039754-8701aeda983b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1955&q=80",
             map: "https://www.massive.pr/wp-content/uploads/2018/01/shutterstock_127728257-1038x576-tender-1024x568.jpg",
             photo: "https://images.unsplash.com/photo-1504544750208-dc0358e63f7f?ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80"
@@ -26,26 +27,41 @@ class ProfilePage extends Component {
         return (
             <React.Fragment>
                 <div className="pageBackground">
-                    <div className="userInfo">
+                    <div className="container">
+                        <div className="userInfo">
+                        <p><img className="profile-img" src={this.state.profilePic} alt="user"/></p>
                         <ul>
-                            <li><img class="profile-pic" src={this.state.profilePic} alt="user"/></li>
                             <li>{this.state.name}</li>
                             <li>@{this.state.userName}</li>
                             <li>{this.state.location}</li>
                             <li>Member Since: {this.state.joinDate}</li>
                             <li>Places Visited: {this.state.placesVisited}</li>
                             <li>Reviews: {this.state.reviews}</li>
-                            <li><img class="map-img" src={this.state.map} alt="map"/></li>
+                            <li>Favorites: {this.state.favorites}</li>
+                        </ul>
+                        </div>
+                        <span>
+                             <img className="map-img" src={this.state.map} alt="map"/>
+                        </span> 
+                    </div>
+                    <Divider />
+                    <div className="userReviews">
+                        <ul>
+                            <li>Restaurant: Joe's</li>
+                            <li>Date Posted: 5/16/19</li>
+                            <li>Review:</li>
+                            <li>My favorite Wonton place in the whole wide world! </li>
+                            <li><img className="review-img" src={this.state.photo} alt="food"/></li>
                         </ul>
                     </div>
                     <Divider />
                     <div className="userReviews">
                         <ul>
-                            <li>Restaurant Name: Wonton-a-Bay </li>
+                            <li>Restaurant: Joe's</li>
                             <li>Date Posted: 5/16/19</li>
-                            <li>Review Comment:</li>
+                            <li>Review:</li>
                             <li>My favorite Wonton place in the whole wide world! </li>
-                            <li><img class="review-img" src={this.state.photo} alt="map"/></li>
+                            <li><img className="review-img" src={this.state.photo} alt="food"/></li>
                         </ul>
                     </div>
                 </div>
