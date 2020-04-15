33bcd254d5be8abef8ef142a34db88f504e8b91e
Jelli <julie.nhudo101@gmail.com>
2020-04-15 00:09:01 -0700

    Attempting to serve static folders from react

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,8 @@
 from flask import Flask
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='', static_folder='build', template_folder='build')
 
 @app.route('/')
-def hello_world():
+def hello():
 	return 'Hello, World!'
 
 if __name__ == "__main__":
