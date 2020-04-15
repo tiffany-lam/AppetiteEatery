bdb39be5ef84ce619df6b9d5e1ae3f6739a9a75e
Ubuntu <ubuntu@ip-10-0-3-105.ec2.internal>
2020-04-15 07:19:26 +0000

    Edited app.py to render our server

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__, static_url_path='', static_folder='build', template_folder='build')
 
 @app.route('/')
 def hello():
-	return 'Hello, World!'
+	return render_template('index.html')
 
 if __name__ == "__main__":
     app.run()
