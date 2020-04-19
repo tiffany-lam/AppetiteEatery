9527c86dd3a2f907eb1f4a524ae2f548e6a397c8
Jelli <julie.nhudo101@gmail.com>
2020-04-19 15:44:00 -0700

    Minimally changed theme

--- frontend/src/pages/contact-us-page/contact-us.component.jsx
+++ frontend/src/pages/contact-us-page/contact-us.component.jsx
@@ -5,6 +5,10 @@ import "./contact-us.styles.scss";
 class ContactUsPage extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      owned: true,
+    };
   }
 
   render() {
@@ -13,10 +17,22 @@ class ContactUsPage extends Component {
         <div className="contact-us-page">
           <h1 className="contact-us-title">What can we do for you today?</h1>
           <form action="" method="post" className="contact-us-form">
+            {this.state.owned ? (
+              <React.Fragment>
+                <label className="name">
+                  <span className="contact-us-label">Full Name</span>
+                  <input type="text" id="name" name="name" required />
+                </label>
+                <label className="email">
+                  <span className="contact-us-label">Email</span>
+                  <input type="text" id="email" name="email" required />
+                </label>
+              </React.Fragment>
+            ) : null}
             <label className="reason">
               <span className="contact-us-label">Reason</span>
-              <select name="reason" id="reason-select">
-                <option value="none" selected disabled hidden>
+              <select name="reason" id="reason-select" required>
+                <option value="" selected disabled hidden>
                   Select an Option
                 </option>
                 <option value="troubleshooting-restaurant-management">
@@ -31,9 +47,14 @@ class ContactUsPage extends Component {
             </label>
             <label className="message">
               <span className="contact-us-label">Message</span>
-              <textarea id="message" name="message" rows="20"></textarea>
+              <textarea
+                id="message"
+                name="message"
+                rows="20"
+                required
+              ></textarea>
             </label>
-            <input type="submit" value="Submit" className="submit" />
+            <input type="submit" value="Submit" />
           </form>
         </div>
       </section>
