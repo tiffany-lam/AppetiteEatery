2c4187fad477af3d71513eb72ef600beb2abd17b
Veronica Sumariyanto <Veronica.Sumariyanto@student.csulb.edu>
2020-04-20 00:30:18 -0700

    added about me and placeholder text for reviews

--- frontend/src/pages/profile-page/profile-page.component.jsx
+++ frontend/src/pages/profile-page/profile-page.component.jsx
@@ -14,7 +14,7 @@ class ProfilePage extends Component {
             userName: "wontom",
             location: "San Francisco, CA",
             joinDate: "1/27/19",
-            placesVisited: "23",
+            placesVisited: "54",
             reviews: "14",
             favorites: "#wontons, #tacos, #ice-cream, #fruit-smoothies, #matcha",
             profilePic: "https://images.unsplash.com/photo-1489481039754-8701aeda983b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1955&q=80",
@@ -30,17 +30,18 @@ class ProfilePage extends Component {
                     <div className="userContainer">
                         <h1>{this.state.name}</h1>
                         <h2>@{this.state.userName}</h2>
+                        <img className="profile-img" src={this.state.profilePic} alt="user"/>
+                        <ul>
+                            <li>{this.state.location}</li>
+                            <li>Member Since: {this.state.joinDate}</li>
+                            <li>Places Visited: {this.state.placesVisited}</li>
+                            <li>Reviews: {this.state.reviews}</li>
+                        </ul>
+                        <h3>About</h3>
+                        <p>Tom Dumpling here. Programmer who loves wontons. I give my honest opinions in my reviews.</p>
                         <div className="userContainer-inner">
-                            <span>
-                                <img className="profile-img" src={this.state.profilePic} alt="user"/>
-                            </span>
-                            <ul>
-                                <li>{this.state.location}</li>
-                                <li>Member Since: {this.state.joinDate}</li>
-                                <li>Places Visited: {this.state.placesVisited}</li>
-                                <li>Reviews: {this.state.reviews}</li>
-                                <li>Favorites: {this.state.favorites}</li>
-                            </ul>
+                            <h3>Favorites</h3> {/* consider these as links in the future*/}
+                            <p>{this.state.favorites}</p>
                             <span>
                                 <p>img goes here</p>
                                 {/* <img className="map-img" src={this.state.map} alt="map"/> */}
@@ -48,24 +49,42 @@ class ProfilePage extends Component {
                         </div>
                     </div>
                     <Divider />
+                    <h2>{this.state.name}'s Reviews</h2>
                     <div className="userReviews">
                         <ul>
                             <li>Restaurant: Joe's</li>
-                            <li>Date Posted: 5/16/19</li>
+                            <li>Date Posted: 3/16/2020</li>
                             <li>Review:</li>
-                            <li>My favorite Wonton place in the whole wide world! </li>
-                            {/* <li><img className="review-img" src={this.state.photo} alt="food"/></li> */}
                         </ul>
+                        <p>My favorite Wonton place in the whole wide world! Customer service is definitely top-notch whenever I visit. The pork dumplings are a MUST. You can have it as an appetizer or with some soup. They also accept Google Pay in case you forget your wallet!</p>
+                        {/* <li><img className="review-img" src={this.state.photo} alt="food"/></li> */}
                     </div>
-                    <Divider />
                     <div className="userReviews">
                         <ul>
-                            <li>Restaurant: Joe's</li>
-                            <li>Date Posted: 5/16/19</li>
+                            <li>Restaurant: Prosperitis</li>
+                            <li>Date Posted: 9/21/2019</li>
                             <li>Review:</li>
-                            <li>My favorite Wonton place in the whole wide world! </li>
-                            {/* <li><img className="review-img" src={this.state.photo} alt="food"/></li> */}
                         </ul>
+                        <p>Came here to fill my smoothie craving one day but left disappointed. Staff took forever to notice me walk in and take my order. I ordered a mango slush but saw that my cup was halfway full? I asked for a new drink but they told me I had to pay for a new one when it wasn't even my fault. The manager clearly needs to train their employees properly. Never coming here again.</p>
+                        {/* <li><img className="review-img" src={this.state.photo} alt="food"/></li> */}
+                    </div>
+                    <div className="userReviews">
+                        <ul>
+                            <li>Restaurant: Engrave</li>
+                            <li>Date Posted: 8/14/2019</li>
+                            <li>Review:</li>
+                        </ul>
+                        <p>I ordered three chicken tacos for carryout. Complimentary chips and salsa were a plus, but I received my food cold. Hopefully next time it won't be like that.</p>
+                        {/* <li><img className="review-img" src={this.state.photo} alt="food"/></li> */}
+                    </div>
+                    <div className="userReviews">
+                        <ul>
+                            <li>Restaurant: Waffle House</li>
+                            <li>Date Posted: 6/3/2019</li>
+                            <li>Review:</li>
+                        </ul>
+                        <p>Visited this place with some coworkers and ordered the Waffle House Special. It consisted of strawberries, powdered sugar, melted marshmellows, and chocolate syrup drizzled on top. Absolutely delicious!</p>
+                        {/* <li><img className="review-img" src={this.state.photo} alt="food"/></li> */}
                     </div>
                 </div>
             </React.Fragment>
