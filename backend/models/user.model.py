e19c8ed463258f012b93a73da6fd31b95a927c90
Jelli <julie.nhudo101@gmail.com>
2020-04-23 22:52:35 -0700

    Prototype of mongo-modified models for database

--- backend/models/user.model.py
+++ backend/models/user.model.py
@@ -1,21 +1,19 @@
 from datetime import datetime
 from mongoengine import Document
-from mongoengine.fields import (
-    StringField, DateTimeField, IntField, ListField GeoPointField, URLField, 
-    ReferenceField, StringField, EmbeddedDocument, EmbeddedDocumentField,
-    BooleanField, ObjectIdField
-)
+from mongoengine.fields import (StringField, ListField, LazyReferenceField,  ObjectIdField, EmailField)
+from review.model import Review
+from restaurant.model import Restaurant
 
 class Client(Document):
     _id: ObjectIdField(primary_key=True)
-    fname: StringField()
-    lname: StringField()
-    email: StringField()
+    fname: StringField(required=True)
+    lname: StringField(required=True)
+    email: EmailField(unique=True, required=True)
 
     meta = {'allow_inheritance': True}
 
 class Patron(Client):
-    reviews: ListField(LazyReferenceField(reverse_delete_rule=CASCADE))
+    reviews: ListField(Review, LazyReferenceField(reverse_delete_rule=CASCADE))
 
 class Owner(Client):
-    restaurantid: LazyReferenceField(reverse_delete_rule=CASCADE)
+    restaurantid: ListField(Restaurant, LazyReferenceField(reverse_delete_rule=CASCADE))
