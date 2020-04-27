3a028399519667aaf26670b8fedd93ca922439cf
jelli <julie.nhudo101@gmail.com>
2020-04-27 01:37:50 -0700

    Routes to save owner/patron successfully added and tested

--- backend/routes/review.py
+++ backend/routes/review.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, Response, Request, jsonify
 from backend.models.reviewmodel import Review
 
+from flask import jsonify
+
 review = Blueprint('review', __name__)
 
 # /api/review/<id>
