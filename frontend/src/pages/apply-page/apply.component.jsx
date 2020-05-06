926fe5c02ee07e4230655cbce721d16abf249fd2
at235am <at235am@gmail.com>
2020-05-06 12:42:26 -0700

    undefined varaible...

--- frontend/src/pages/apply-page/apply.component.jsx
+++ frontend/src/pages/apply-page/apply.component.jsx
@@ -78,6 +78,8 @@ const stateAbbreviations = [
 ];
 
 const ApplyPage = ({ userAuth, ...props }) => {
+  const browserHistory = useHistory();
+
   const [restaurantName, setRestaurantName] = useState("");
   const [description, setDescription] = useState("");
   const [dateOpened, setDateOpened] = useState("");
@@ -143,9 +145,7 @@ const ApplyPage = ({ userAuth, ...props }) => {
         console.log(res.data);
 
         // let id = res.data._id.$oid;
-        submitImages(res.data._id.$oid).then(() => {
-          browserHistory.push("/my-restaurant");
-        });
+        submitImages(res.data._id.$oid);
       })
       .catch((err) => {
         console.log(textData);
@@ -165,14 +165,23 @@ const ApplyPage = ({ userAuth, ...props }) => {
       formData.append("menu[]", menus[i]);
     }
 
-    return await axios.post(
+    let res = await axios.post(
       `${BASE_API_URL}/restaurant/img-upload/${restaurantId}`,
       formData
     );
+
+    browserHistory.push("/my-restaurants");
+
+    return res;
   };
 
   return (
-    <div className="apply-page-container">
+    <div
+      className="apply-page-container"
+      // onClick={() => {
+      //   browserHistory.push("/my-restaurants");
+      // }}
+    >
       <h1 className="apply-form-header input-override">
         Submit your restaurant!
       </h1>
