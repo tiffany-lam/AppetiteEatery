045d66c417dc84398dda7bcb772bd44a85f9cce5
Jelli <julie.nhudo101@gmail.com>
2020-05-08 14:13:34 -0700

    Map container changes

--- frontend/src/components/map-container/map-container.component.jsx
+++ frontend/src/components/map-container/map-container.component.jsx
@@ -11,37 +11,17 @@ const mapStyles = {
   position: "relative",
 };
 
-const MapContainer = ({ latitude, longitude, ...props }) => {
-  console.log(Number(latitude));
-  console.log(Number(longitude));
-  // const lati = -34.95;
-  // const longi = 138.55;
-  const lati = latitude;
-  const longi = longitude;
-  const center = {
-    lat: latitude,
-    lng: longitude,
-  };
-  return (
-    <Map
-      google={props.google}
-      zoom={14}
-      // style={mapStyles}
-      initialCenter={center}
-      // center={{
-      //   // lat: 71,
-      //   // lng: 71,
-      //   lat: lati,
-      //   lng: longi,
-      // }}
-      // initialCenter={{
-      //   lat: latitude,
-      //   lng: longitude,
-      //   // lat: latitude,
-      //   // lng: longitude,
-      // }}
-    />
-  );
+const MapContainer = (props) => {
+  console.log(latitude);
+  console.log(longitude);
+  <Map
+    google={props.google}
+    zoom={14}
+    initialCenter={{
+      lat: props.latitude,
+      lng: props.longitude,
+    }}
+  />;
 };
 
 export default GoogleApiWrapper({
