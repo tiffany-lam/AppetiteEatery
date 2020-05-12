3e617e750f952e89979b88cdb7a97bd2491a92e7
Veronica Sumariyanto <Veronica.Sumariyanto@student.csulb.edu>
2020-05-12 15:14:14 -0700

    user avatar and map component show - began set up for editing info

--- frontend/src/pages/profile-page/profile-page.component.jsx
+++ frontend/src/pages/profile-page/profile-page.component.jsx
@@ -11,7 +11,6 @@ import {
   updateCurrentUser,
 } from "../../redux/user/user.actions";
 
-
 // Custom Style Sheet
 import "./profile-page.styles.scss";
 
@@ -24,6 +23,7 @@ import CustomButton from "../../components/custom-button/custom-button.component
 
 import CreateIcon from "@material-ui/icons/Create";
 import DoneIcon from "@material-ui/icons/Done";
+import FormInput from "../../components/form-input/form-input.component";
 
 class ProfilePage extends Component {
   constructor(props) {
@@ -32,11 +32,10 @@ class ProfilePage extends Component {
       fname: "",
       lname: "",
       email: "",
-      type: "Restaurant Patron",
       reviewCount: "",
       tags: [],
-      avatar:
-        "https://images.unsplash.com/photo-1489481039754-8701aeda983b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1955&q=80",
+      avatar: "",
+      //   "https://images.unsplash.com/photo-1489481039754-8701aeda983b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1955&q=80",
       reviews: [],
     };
   }
@@ -47,10 +46,7 @@ class ProfilePage extends Component {
     // Request to get patron reviews
     let res = await axios.get(`${BASE_API_URL}/user/getPatronReviews/${this.props.userAuth.uid}`);
     console.log("check meeee");
-    // console.log(res);
-    // console.log(res.data);
-    // console.log("reviewws??", res.data.reviews.length);
-    console.log(process.env);
+    // console.log(process.env);
 
     this.setState({ reviews: res.data.reviews });
     this.setState({ reviewCount: res.data.reviews.length });
@@ -86,52 +82,60 @@ class ProfilePage extends Component {
         return (
           <React.Fragment>
             <section className="profile-page-container">
-              <section className="userContainer">
-                {/* <button type="button" onClick={(e) => {
-                  // console.log(this.props.userAuth);
-                  // console.log(this.props.currentUser);
-                  console.log(this.state);
-                }}>
-                    test
-                </button> */}
-                <h1>{this.props.currentUser.fname} {this.props.currentUser.lname}</h1>
-                <CustomButton type="button" icon={<CreateIcon />} className="profile-button">
-                  Edit My Info
-                </CustomButton>
-                <h2 id="toggle-mini">
-                  {this.props.currentUser.email}
-                 <CustomButton type="button" icon={<CreateIcon />} className="mini-icon"></CustomButton></h2>
-                {/* <p id="accountType">{this.state.type}</p> */}
-                <div className="userContainer-inner">
-                    <div id="col1">
-                      <img
-                        className="profile-img"
-                        src={this.state.avatar}
-                        alt="user"
-                      />
-                      <h3 id="toggle-mini">Favorites:
-                      <CustomButton type="button" icon={<CreateIcon />} className="mini-icon"></CustomButton></h3>
-                      <ul id="favorites">{tags}</ul>
-                    </div>
-                    <div id="col2">
-                      <h3>About Me</h3> 
-                      <p>
-                        {this.props.currentUser.about}
-                      </p>
-                      <h3>Favorite Restaurant</h3>
-                      <div id="checkIn">
-                        <MapContainer />
+              <form action="" method="put" id="manage-profile">
+                <section className="userContainer">
+                  <h1>{this.props.currentUser.fname} {this.props.currentUser.lname}</h1>
+                  <CustomButton type="button" icon={<CreateIcon />} className="profile-button">
+                    Edit My Profile
+                  </CustomButton>
+                  <h2 id="toggle-mini">{this.props.currentUser.email}</h2>
+                  <div className="userContainer-inner">
+                    {/* <fieldset form="manage-profile1" classname=""> */}
+                      <div id="col1">
+                        <img
+                          className="profile-img"
+                          src={`${BASE_API_URL}/img-get?url=${this.props.currentUser.avatar}`}
+                          alt="user"
+                        />
+                        <h3 id="toggle-mini">Favorites
+                        <CustomButton type="button" icon={<CreateIcon />} className="mini-icon"></CustomButton></h3>
+                        <ul id="favorites">{tags}</ul>
                       </div>
-                    </div>
-                  {/* <div className="favorites">
-                    <h3>Favorites:</h3>
-                    <p>{this.state.tags}</p>
-                  </div> */}
-                </div>
-                <CustomButton type="button" icon={<DoneIcon />} className="profile-save-button" id="hidden">
-                  Save Changes
-                </CustomButton>
-              </section>
+                      <div id="col2">
+                        <h3>About Me</h3> 
+                        <FormInput
+                          type="textarea"
+                          htmlFor="about"
+                          label="about"
+                          value={this.props.currentUser.about}
+                          // handleChange={(e) => {
+                          //   setProfile({
+                          //     ...this.props.currentUser,
+                          //     about: e.target.value,
+                          //   });
+                          // }}
+                          maxLength="250"
+                          additionalInfo="(max: 250 characters)"
+                          // disabled={editInput !== "description"}
+                        />
+                        {/* <p>
+                          {this.props.currentUser.about}
+                        </p> */}
+                        <h3>Favorite Restaurant</h3>
+                        <div id="checkIn">
+                          <MapContainer 
+                            longitude={33.6714426}
+                            latitude={-117.7910193}
+                          />
+                        </div>
+                      </div>
+                    {/* </fieldset> */}
+                  </div>
+                  <CustomButton type="button" icon={<DoneIcon />} className="profile-save-button" id="hidden">
+                    Save Changes
+                  </CustomButton>
+                </section>
+              </form>
               <section className="userReviews">
                 <h2>{this.props.currentUser.fname} {this.props.currentUser.lname}'s Reviews</h2>
                 <h3>Total Reviews: {this.state.reviewCount}</h3>
