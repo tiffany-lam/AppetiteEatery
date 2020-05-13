0a8605c1e5a8dd511202278b78056d295c12be81
at235am <at235am@gmail.com>
2020-05-13 10:56:39 -0700

    small changes

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -4,7 +4,9 @@ import thunk from "redux-thunk";
 
 import rootReducer from "./root.reducer";
 
-const middlewares = [logger, thunk];
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV === "development") middlewares.push(logger);
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
